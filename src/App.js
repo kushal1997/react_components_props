09aad5a388cf49fe9dd1e21a083bcb5cc7bcea35
kushal1997 <kushalrao103@gmail.com>
2023-03-22 16:22:53 +0530

    added code from previous file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(){
+  const [tasks,setTasks]=useState([
+        {id: 1, name:"Record Videos", completed:false },
+        {id: 2, name:"Listem Music", completed:true},
+        {id: 3, name:"Study", completed:false}
+      ]);
+  const [show,setShow]=useState(true);
+
+  function handleDelete(id){
+    setTasks(tasks.filter(task => id!== task.id));
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Task List</h1>
+        <ul>
+        <button onClick={()=> setShow(!show)}>Toggle</button>
+        {
+          show && tasks.map((task) =>(
+            <li key={task.id} className={task.completed? "completed":"incomplete"}>
+              <span>{task.id} - {task.name}</span>
+              <button onClick={()=> handleDelete(task.id)}>Delete</button>
+            </li>
+          ))
+        }
+          
+        </ul>
     </div>
   );
 }
-
 export default App;
